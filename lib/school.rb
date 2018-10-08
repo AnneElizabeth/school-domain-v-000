9f8424941aac28a55f3610799348197d228425b6
Anne Edmondson <edmondson.anne@gmail.com>
2018-10-08 02:35:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  
+  def initialize
+   @name = name
+   @roster = {}
+  end
+  
+  def roster
+    @roster 
+  end
+  
+  def add_student(name, grade)
+    
+  end
+end
